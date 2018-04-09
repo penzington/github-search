70d0ef6e681cb74e7b17aa409525591358e35719
Maciek Pekala <mp@issuu.com>
2018-04-09 23:34:05 +0200

    Split container and presentational components for Home

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,44 @@
 import React from "react";
 import { Connect, query } from "urql";
 
-const TodoQuery = `
+const ViewerQuery = `
 {
   viewer {
-    login
     avatarUrl
     bio
     company
     createdAt
     location
+    login
+    name
     url
     websiteUrl
   }
 }
 `;
 
-const Home = () => (
-  <Connect query={query(TodoQuery)}>
-    {({ loaded, fetching, refetch, data, error, addTodo }) => {
-      return loaded ? <div>${JSON.stringify(data)}</div> : "loading...";
+const Home = ({ name, login, location, url, company, bio, avatarUrl }) => (
+  <div>
+    <h1>
+      {name}{" "}
+      <a href={url}>
+        <code>{login}</code>
+      </a>
+    </h1>
+    <img alt={name} src={avatarUrl} />
+    <p>{bio}</p>
+    <p>
+      Works at {company}. Lives in {location}.
+    </p>
+  </div>
+);
+
+const HomeContainer = () => (
+  <Connect query={query(ViewerQuery)}>
+    {({ loaded, data }) => {
+      return loaded ? <Home {...data.viewer} /> : "loading...";
     }}
   </Connect>
 );
 
-export default Home;
+export default HomeContainer;
