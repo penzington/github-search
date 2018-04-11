658d6c19a90bd2686f6d76f9cd0be3816511f441
Maciek Pekala <m@peka.la>
2018-04-11 23:21:05 +0200

    Add routing to the App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { Provider, Client } from "urql";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import Home from "./Home";
-import Search from "./Search";
+import Login from "./Login";
+import Profile from "./Profile";
 
 const getClient = token =>
   new Client({
@@ -13,10 +20,29 @@ const getClient = token =>
     }
   });
 
+const ProtectedRoute = ({ component: Component, isLoggedIn, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      isLoggedIn ? <Component {...props} /> : <Redirect to="/login" />
+    }
+  />
+);
+
 export const App = ({ token }) => (
   <Provider client={getClient(token)}>
-    <Home />
-    <Search />
+    <Router>
+      <Switch>
+        <ProtectedRoute exact path="/" isLoggedIn={!!token} component={Home} />
+        <ProtectedRoute
+          path={`/profiles/:login`}
+          isLoggedIn={!!token}
+          component={Profile}
+        />
+        <Route path="/login" component={Login} />
+        <Route component={() => "404 :("} />
+      </Switch>
+    </Router>
   </Provider>
 );
 
---
+++ src/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Login = () => (
+  <div>
+    <a href="https://TBA/login">login</a>
+  </div>
+);
+
+export default Login;
