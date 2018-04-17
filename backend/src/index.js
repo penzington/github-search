556b63f6d95d5c7d7343d6fe858a087e17aeebba
Maciek Pekala <m@peka.la>
2018-04-17 22:24:12 +0200

    Move source files to src dir in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ const {
   logUsersGetHandler,
   logUserGetHandler
 } = require("./log-handler");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(morgan("tiny"));
 app.use(bodyParser.text());
